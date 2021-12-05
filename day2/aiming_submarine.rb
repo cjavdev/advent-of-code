17eeac278f587f232c816ce2a1bbfa2003da5d7c
cjavdev <cjavilla@gmail.com>
2021-12-04 16:08:50 -0800

    Day 2 - Part 2

---
+++ day2/aiming_submarine.rb
@@ -0,0 +1,39 @@
+class AimingSubmarine
+  attr_reader :aim, :depth, :horizontal_position
+
+  def initialize(&blk)
+    @aim = 0
+    @horizontal_position = 0
+    @depth = 0
+
+    if block_given?
+      instance_eval(&blk)
+    end
+  end
+
+  def forward(n)
+    @horizontal_position += n
+    @depth += (aim * n)
+  end
+
+  def down(n)
+    @aim += n
+  end
+
+  def up(n)
+    @aim -= n
+  end
+
+  def location
+    horizontal_position * depth
+  end
+end
+
+if __FILE__ == $0
+  # Part 1
+  commands = File.read(ARGV.first)
+  submarine = AimingSubmarine.new do
+    eval(commands)
+  end
+  puts submarine.location
+end
--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -1,4 +1,25 @@
 require '../day2/submarine.rb'
+require '../day2/aiming_submarine.rb'
+
+
+RSpec.describe AimingSubmarine do
+  it 'up and down control aim' do
+    submarine = AimingSubmarine.new
+    submarine.down(2)
+    expect(submarine.aim).to eq(2)
+    submarine.up(3)
+    expect(submarine.aim).to eq(-1)
+  end
+
+  it 'forward increases position and depth' do
+    submarine = AimingSubmarine.new
+    submarine.down(2)
+    submarine.forward(2)
+    expect(submarine.horizontal_position).to eq(2)
+    expect(submarine.depth).to eq(4)
+  end
+
+end
 
 RSpec.describe Submarine do
 
@@ -44,3 +65,4 @@ RSpec.describe Submarine do
   end
 
 end
+
