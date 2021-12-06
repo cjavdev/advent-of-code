53899df56f847beea1f6b20675209f0db46fb53c
cjavdev <cjavilla@gmail.com>
2021-12-05 19:44:46 -0800

    Add day3 part 2

--- day3/diagnostic.rb
+++ day3/diagnostic.rb
@@ -1,52 +1,123 @@
 require 'byebug'
 
-def width(measurements)
-  # Convert the numbers into strings of binary digits
-  measurement_bits = measurements.map { |m| m.to_s(2) }
-
-  # Find the widest number of bits
-  measurement_bits.max_by(&:length).length
+class Array
+  def median
+    return if length == 0
+    middle_index = (length / 2).ceil
+    puts "Len: #{length} mi: #{middle_index}"
+    self[middle_index]
+  end
 end
 
-def gamma_rate(measurements)
-  # Convert the numbers into strings of binary digits
-  measurement_bits = measurements.map { |m| m.to_s(2) }
+class Diagnostic
+  attr_reader :measurements
 
-  # Find the widest number of bits
-  width = width(measurements)
-
-  # Create a counter for each bit position
-  counter = Array.new(width, 0)
-
-  measurement_bits.each do |m|
-    # reverse, so that we're always starting with the least significant bit
-    m.chars.reverse.each_with_index do |c, i|
-      # If the bit is a 1, increment the counter
-      counter[i] += 1 if c == '1'
-    end
+  def initialize(measurements, gamma_rate=nil)
+    @measurements = measurements.sort
+    @gamma_rate = gamma_rate
   end
 
-  # Negative numbers mean 0 is most common
-  # Positive numbers mean 1 is most common
-  counter
-    .reverse
-    .map {|x| x > (measurements.count / 2) && 1 || 0 }
-    .join.to_i(2)
-end
+  def gamma_rate
+    half_measurement_count = measurements.count / 2
+    # Negative numbers mean 0 is most common
+    # Positive numbers mean 1 is most common
 
-def epsilon_rate(n, width)
-  n ^ (["1"] * width).join.to_i(2)
+    @gamma_rate ||= bit_frequency
+      .map {|x| x > half_measurement_count && 1 || 0 }
+      .join
+      .to_i(2)
+  end
+
+  def epsilon_rate
+    gamma_rate ^ (["1"] * max_width).join.to_i(2)
+  end
+
+  def oxygen_rate
+    oxygen_rate_bits.to_i(2)
+  end
+
+  def co2_rate
+    co2_rate_bits.to_i(2)
+  end
+
+  protected
+
+  def gamma_bits
+    @_gb ||= gamma_rate.to_s(2).rjust(max_width, '0')
+  end
+
+  def epsilon_bits
+    @_eb ||= epsilon_rate.to_s(2).rjust(max_width, '0')
+  end
+
+  private
+
+  def measurement_bits
+    # Convert the numbers into strings of binary digits
+    @_mb ||= measurements.map { |m| m.to_s(2).rjust(max_width, '0') }
+  end
+
+  def max_width
+    @max_width ||= measurements.map {_1.to_s(2)}.max_by(&:length).length
+  end
+
+  def bit_frequency
+    # Create a counter for each bit position
+    counter = Array.new(max_width, 0)
+
+    measurement_bits.each do |m|
+      # reverse, so that we're always starting with the least significant bit
+      m.chars.reverse.each_with_index do |c, i|
+        # If the bit is a 1, increment the counter
+        counter[i] += 1 if c == '1'
+      end
+    end
+
+    counter.reverse
+  end
+
+  def oxygen_rate_bits
+    included_measurements = measurement_bits
+
+    max_width.times do |i|
+      mid = included_measurements.median
+      # if the bit at the mid point is 1, keep all that are greater than or equal to
+      left, right = included_measurements.partition do |el|
+        el[i] == mid[i]
+      end
+
+      next if included_measurements.length == 1
+      included_measurements = left
+    end
+
+    included_measurements.first
+  end
+
+  def co2_rate_bits
+    included_measurements = measurement_bits
+
+    max_width.times do |i|
+      mid = included_measurements.median
+      # if the bit at the mid point is 1, keep all that are greater than or equal to
+      left, right = included_measurements.partition do |el|
+        el[i] != mid[i]
+      end
+
+      next if included_measurements.length == 1
+      included_measurements = left
+    end
+
+    included_measurements.first
+  end
 end
 
 if __FILE__ == $0
   # Part 1
-  # puts 0b000101011101
-
+  # Manually found gamma - 0b000101011101
   measurements = File.readlines(ARGV.first).map {|x|x.to_i(2)}
-  p measurements.length
-  g = gamma_rate(measurements)
-  e = epsilon_rate(g, width(measurements))
-  puts g
-  puts e
-  puts g * e
+  diagnostic = Diagnostic.new(measurements)
+  o2 = diagnostic.oxygen_rate
+  diagnostic = Diagnostic.new(measurements)
+  co2 = diagnostic.co2_rate
+  puts o2 * co2
 end
--- spec/day3_spec.rb
+++ spec/day3_spec.rb
@@ -1,7 +1,16 @@
 require '../day3/diagnostic.rb'
 require 'byebug'
 
+def gamma_rate(input)
+  Diagnostic.new(input).gamma_rate
+end
+
+def epsilon_rate(input)
+  Diagnostic.new([input], gamma_rate=input).epsilon_rate
+end
+
 RSpec.describe 'diagnostic' do
+
   it 'calculates the gamma rate for a small list of readings' do
     gamma = gamma_rate([
       0b00100,
@@ -32,5 +41,47 @@ RSpec.describe 'diagnostic' do
   it 'calculates the epsilon rate' do
     epsilon = epsilon_rate(0b10110)
     expect(epsilon).to eq(0b01001)
+  end
+
+  it 'calculates the oxygen rate for 3 measurements' do
+    diagnostic = Diagnostic.new([
+      0b00100,
+      0b10110,
+      0b11110,
+    ])
+    expect(diagnostic.oxygen_rate).to eq(0b11110)
+  end
+
+  it 'calculates the oxygen rate for the example input' do
+    diagnostic = Diagnostic.new([
+      0b00100,
+      0b11110,
+      0b10110,
+      0b10111,
+      0b10101,
+      0b01111,
+      0b00111,
+      0b11100,
+      0b10000,
+      0b11001,
+      0b00010,
+      0b01010
+    ])
+    expect(diagnostic.oxygen_rate).to eq(23)
+  end
+
+  it 'Array#median finds median' do
+    a = [1, 2, 3, 4, 5]
+    expect(a.median).to eq(3)
+    a = [1, 2, 3, 4]
+    expect(a.median).to eq(3)
+    a = [1, 2, 3]
+    expect(a.median).to eq(2)
+    a = [1, 2]
+    expect(a.median).to eq(2)
+    a = [1]
+    expect(a.median).to eq(1)
+    a = []
+    expect(a.median).to eq(nil)
   end
 end
