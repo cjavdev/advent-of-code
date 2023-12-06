ecad56a6fe8965c0e2c021120bb3f4c927665f1b
CJ Avilla <cjavilla@gmail.com>
2023-12-06 09:21:05 -0500

    Add day 06

--- 2023/day06/main.rb
+++ 2023/day06/main.rb
@@ -1,5 +1,13 @@
-data = DATA.readlines
-# data = File.readlines(ARGV.first)
+races = [[7, 9], [15, 40], [30, 200]]
+races = [[71530, 940200]]
 
-
-__END__
+races.inject(1) do |acc, (t, record)|
+  c = 0
+  t.times do |i|
+    if (i * (t-i)) > record
+      c += 1
+    end
+  end
+  acc * c
+end => r
+p r
