fd74fd2e71a6b1560e21b5ea54b42574c24a505f
CJ Avilla <cjavilla@gmail.com>
2023-12-18 08:16:59 -0500

    Add day 18

--- 2023/day17/main.rb
+++ 2023/day17/main.rb
@@ -1,5 +1,87 @@
-data = DATA.readlines
-# data = File.readlines(ARGV.first)
+input = <<~INPUT
+2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533
+INPUT
+input = <<~INPUT
+1111
+9991
+9991
+9991
+INPUT
 
+require 'set'
 
-__END__
+DIRS = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+].freeze
+
+def find(blocks, max_in_row)
+  # Start at 0, 0 going either right or down
+  # with a heat of 0
+  states = []
+  states << [0, 0, 0, 0, 1, 1] # heat, x, y, dx, dy, blocks_in_row
+  states << [0, 0, 0, 1, 0, 1] #
+  visited = Set.new
+
+  while !states.empty?
+    current = states.sort!.shift
+    wo_heat = current[1..]
+    if visited.include?(wo_heat)
+      next
+    end
+    visited << wo_heat
+
+    heat, x, y, dx, dy, blocks_in_row = current
+    nx, ny = x + dx, y + dy
+
+    # If we'd fall off the grid, go to the next state
+    if nx < 0 || ny < 0 || nx >= blocks.length || ny >= blocks.first.length
+      next
+    end
+
+    new_heat = heat + blocks[nx][ny]
+
+    # If we're at the bottom right corner, return the new heat
+    if nx == blocks.length - 1 && ny == blocks.first.length - 1
+      return new_heat
+    end
+
+    # Go through all possible directions to find new states
+    DIRS.each do |ddx, ddy|
+      # don't go back to the same spot
+      if [ddx + dx, ddy + dy] == [0, 0]
+        next
+      end
+
+      if [ddx, ddy] == [dx, dy]
+        new_blocks_in_row = blocks_in_row + 1
+      else
+        new_blocks_in_row = 1
+      end
+
+      if new_blocks_in_row > max_in_row
+        next
+      end
+
+      states << [new_heat, nx, ny, ddx, ddy, new_blocks_in_row]
+    end
+  end
+end
+
+input = DATA.read
+blocks = input.split("\n").map {|line| line.chars.map(&:to_i) }
+p "Min heat loss: #{find(blocks, 3)}"
--- 2023/day18/main.rb
+++ 2023/day18/main.rb
@@ -1,5 +1,68 @@
-data = DATA.readlines
-# data = File.readlines(ARGV.first)
+require 'set'
 
+input = <<~INPUT
+R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)
+INPUT
+# input = DATA.read
+ins = input.split("\n").map(&:split)
 
-__END__
+DIRS = {
+  "U" => [-1, 0],
+  "D" => [1, 0],
+  "L" => [0, -1],
+  "R" => [0, 1]
+}
+
+DIRS2 = {
+  "3" => [-1, 0],
+  "1" => [1, 0],
+  "2" => [0, -1],
+  "0" => [0, 1]
+}
+
+ins = ins.map do |dir, steps, color|
+  [DIRS[dir], steps.to_i, color]
+end
+
+ins = ins.map do |dir, steps, color|
+  co = color.gsub("(", "").gsub(")", "").gsub("#", "")
+  cs = co[0...5].to_i(16)
+  dir2 = co[5]
+  # [DIRS[dir], steps.to_i, color]#
+  [DIRS2[dir2], cs]
+end
+# p ins
+pos = [0, 0]
+
+wall = [pos]
+
+ins.each do |(dx, dy), s|
+  s.times do
+    pos = pos[0] + dx, pos[1] + dy
+    wall << pos.dup
+  end
+end
+
+p wall.length
+wall
+  .each_cons(2)
+  .sum do |(x1, y1), (x2,y2)|
+    x1 * y2 - x2 * y1
+  end
+    .abs
+    .fdiv(2) => a
+
+p (a - wall.length/2) + wall.length
