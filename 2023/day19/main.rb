b1e9c0e788424f1d5d8de5af9babbd97106f606a
CJ Avilla <cjavilla@gmail.com>
2023-12-20 08:46:06 -0500

    Add day 20

--- 2023/day19/main.rb
+++ 2023/day19/main.rb
@@ -1,5 +1,122 @@
-data = DATA.readlines
-# data = File.readlines(ARGV.first)
+require 'rspec/autorun'
+require 'byebug'
+require 'set'
+
+input = <<~INPUT
+px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}
+INPUT
+
+# input = DATA.read
+workflows, parts = input.split("\n\n")
+workflows = workflows.split(/\n/).map do |wf|
+  name, *rules = wf.split(/[{},]/)
+  rules = rules.map do |rule|
+    if rule.include?(':')
+      condition, dest = rule.split(':')
+      condition = [
+        condition[0].to_sym,
+        condition[1].to_sym,
+        condition[2..-1].to_i
+      ]
+      [condition, dest.to_sym]
+    else
+      rule.to_sym
+    end
+  end
+  [name.to_sym, rules]
+end.to_h
+
+# parts = parts.split(/\n/).map do |part|
+#   part.gsub(/[{}]/, '').split(/,/).map do |comp|
+#     var, val = comp.split('=')
+#     [var, val.to_i]
+#   end.to_h
+# end
+#
+# p parts
+
+# parts.filter_map do |part|
+#   if process(part, workflows, "in")
+#     p part.values.sum
+#   end
+# end.sum => a
+# p a
 
 
-__END__
+def deep_dup(obj)
+  Marshal.load(Marshal.dump(obj))
+end
+
+def get_ranges(workflows)
+  accepted_rules = Set.new
+  default_constraints = { x: [0, 4000], m: [0, 4000], a: [0, 4000], s: [0, 4000] }
+
+  queue = [[:in, { x: [0, 4000], m: [0, 4000], a: [0, 4000], s: [0, 4000] }]]
+
+  while queue.any?
+    current = queue.shift
+    k, ranges = current
+    ranges = deep_dup(ranges)
+
+    if k == :A
+      accepted_rules << ranges
+      next
+    elsif k == :R
+      next
+    end
+
+
+    workflows[k].each do |rule|
+      if rule.is_a?(Array)
+        (var, op, val), dest = rule
+        new_range = ranges
+
+        # Add a new range
+        if op == :<
+          if val <= default_constraints[var][0]
+            next
+          end
+
+          # Set the default constraints max to the value.
+          default_constraints[var][1] = val
+
+          # Set the new state's min to the value - 1
+          new_range[var][0] = val - 1
+        elsif op == :>
+          if val >= default_constraints[var][1]
+            next
+          end
+
+          # Set the default constraints min to the value.
+          default_constraints[var][0] = val
+
+          # Set the new state's max to the value + 1
+          new_range[var][1] = val + 1
+        end
+        queue << [dest, new_range]
+      else
+        queue << [rule, ranges.dup]
+      end
+    end
+  end
+
+  p accepted_rules
+end
+
+get_ranges(workflows)
--- 2023/day20/main.rb
+++ 2023/day20/main.rb
@@ -1,5 +1,264 @@
-data = DATA.readlines
-# data = File.readlines(ARGV.first)
+puts 'Hello, World!'
+require 'byebug'
 
+input = <<~INPUT
+broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a
+INPUT
+
+input = <<~INPUT
+broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output
+INPUT
+
+input = DATA.read
+
+class Node
+  def self.parse(line, nodes)
+    n, dest = line.split(/ -> /)
+    dest = dest.split(/, /).map(&:to_sym)
+    kind = nil
+    if n[0] == '%'
+      name = n[1..-1].to_sym
+      nodes[name] = FlipFlop.new(name, dest, nodes)
+    elsif n[0] == '&'
+      name = n[1..-1].to_sym
+      nodes[name] = Conjunction.new(name, dest, nodes)
+    elsif n == 'broadcaster'
+      name = n.to_sym
+      nodes[name] = Broadcaster.new(name, dest, nodes)
+    else
+      name = n.to_sym
+      nodes[name] = Node.new(name, dest, nodes)
+    end
+  end
+
+  attr_reader :name, :destinations, :nodes, :low_count, :high_count
+
+  def initialize(name, destinations, nodes)
+    @name = name
+    @destinations = destinations
+    @nodes = nodes
+    @low_count = 0
+    @high_count = 0
+  end
+
+  def low(from)
+    @low_count += 1
+    []
+  end
+
+  def high(from)
+    @high_count += 1
+    []
+  end
+
+  def get_low
+    destinations.map do |dest|
+      # puts "#{name} -low-> #{dest}"
+      [dest, :low, name]
+    end
+  end
+
+  def get_high
+    destinations.map do |dest|
+      # puts "#{name} -high-> #{dest}"
+      [dest, :high, name]
+    end
+  end
+end
+
+class Rx < Node
+  def low(from)
+    raise "Rx sent low signal and is starting machine!"
+  end
+end
+
+class FlipFlop < Node
+  def initialize(name, destinations, nodes)
+    @on = false
+    super(name, destinations, nodes)
+  end
+
+  def on?
+    @on
+  end
+
+  def low(from)
+    super
+
+    if on?
+      @on = false
+      get_low
+    else
+      @on = true
+      get_high
+    end
+  end
+end
+
+class Conjunction < Node
+  def initialize(name, destinations, nodes)
+    @inputs = Hash.new {|h, k| h[k] = :low}
+    super(name, destinations, nodes)
+  end
+
+  def input_nodes
+    @input_nodes ||= nodes.select do |k, v|
+      v.destinations.include?(name)
+    end
+  end
+
+  def all_high?
+    input_nodes.all? do |k, v|
+      @inputs[k] == :high
+    end
+  end
+
+  def high(from)
+    @inputs[from] = :high
+    super
+    go
+  end
+
+  def low(from)
+    @inputs[from] = :low
+    super
+    go
+  end
+
+  def go
+    if all_high?
+      get_low
+    else
+      get_high
+    end
+  end
+end
+
+class Broadcaster < Node
+  def low(from)
+    super
+    get_low
+  end
+end
+
+nodes = {}
+input.split(/\n/).each do |line|
+  Node.parse(line, nodes)
+end
+unknown = []
+nodes.each do |k, v|
+  v.destinations.each do |dest|
+    if nodes[dest].nil?
+      unknown << dest
+    end
+  end
+end
+unknown.each do |dest|
+  if dest == :rx
+    nodes[dest] = Rx.new(dest, [], nodes)
+  else
+    nodes[dest] = Node.new(dest, [], nodes)
+  end
+end
+p unknown
+
+def button(nodes)
+  q = [[:broadcaster, :low, :from]]
+  while !q.empty?
+    name, status, from = q.shift
+    q += nodes[name].send(status, from)
+  end
+end
+
+# p nodes[:rx].low(nil)
+
+# Part 1
+# 1000.times do
+#   button(nodes)
+# end
+# lc = nodes.sum { |k, v| v.low_count }
+# hc = nodes.sum { |k, v| v.high_count }
+# puts "Low counts: #{lc}, High counts: #{hc}, Total: #{hc * lc}"
+
+# Part 2
+begin
+  button_presses = 0
+  while true
+    button_presses += 1
+    button(nodes)
+    if button_presses % 1000 == 0
+      puts "Button presses: #{button_presses}"
+    end
+  end
+rescue => e
+  puts "Button presses: #{button_presses}"
+  p e
+end
 
 __END__
+%np -> vn
+&lv -> rx
+%rt -> ns
+%th -> bc
+%gt -> rt, db
+%zf -> db, np
+%sg -> fs, gr
+%vn -> db, zj
+%qh -> ms, lz
+%rv -> rj, vc
+%br -> lz, qh
+%pc -> jq, vc
+%dk -> xl
+%qq -> th, gr
+%ns -> xv
+&vc -> gl, tv, pc, qd, tn, dg
+%bd -> lz, vm
+%ms -> lz, bd
+%dg -> rv
+%cf -> vc
+%kc -> cq, db
+%ds -> dk, lz
+%zj -> kc
+%qm -> db, zf
+%gl -> qd
+%hf -> db
+%hx -> px, gr
+%fk -> tv, vc
+%tp -> ld
+%gg -> rq, gr
+%xl -> gj, lz
+%vm -> lz
+%qf -> lz, vr
+%px -> qq
+%fs -> tp
+%bc -> cd, gr
+%vr -> xz, lz
+%xv -> qm, db
+%rq -> gr
+%cq -> hf, db
+&lz -> dt, dk, qf
+&gr -> tp, fs, px, st, th, sg
+&st -> lv
+&tn -> lv
+%xz -> ds, lz
+&hh -> lv
+&db -> np, gt, zj, ns, hh, rt
+%qd -> dg
+%jq -> vc, fk
+%jp -> cf, vc
+%rj -> jp, vc
+%tv -> kz
+%cd -> gg, gr
+&dt -> lv
+%ld -> hx, gr
+%kz -> gl, vc
+broadcaster -> pc, sg, qf, gt
+%gj -> lz, br
