cf34fbf786c68732d049c9c65d197d5a94bf6b53
CJ Avilla <cjavilla@gmail.com>
2024-12-01 13:23:20 -0500

    day 1 2024

---
+++ 2024/day01/main.rb
@@ -0,0 +1,25 @@
+exmple = <<~EXAMPLE
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+EXAMPLE
+
+input = File.open("./2024/day01/input").read
+# input = exmple
+
+left, right = input
+  .split("\n")
+  .map { |line| line.split.map(&:to_i) }
+  .transpose
+
+left = left.sort
+right = right.sort
+
+# PART 1
+puts left.zip(right).map { |l, r| (l - r).abs }.sum
+
+# PART 2
+puts left.map { |l| right.count { |r| r == l } * l }.sum
